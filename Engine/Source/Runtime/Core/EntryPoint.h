7d0039cd336cadb04838db209ac82aa4455a9641
AlexMultan <olek.multan06@gmail.com>
2026-08-24 21:22:43 +0200

    feat(platform): force dedicated GPU selection on Windows and Linux
    - Export NvOptimusEnablement and AmdPowerXpressRequestHighPerformance symbols on Win64 to force dedicated GPU over iGPU - Add Linux dedicated GPU forcing via NV_PRIME_RENDER_OFFLOAD and DRI_PRIME environment variables with process re-exec - Necessary workaround as OpenGL does not support explicit physical device selection unlike DirectX and Vulkan

--- Engine/Source/Runtime/Core/EntryPoint.h
+++ Engine/Source/Runtime/Core/EntryPoint.h
@@ -1,14 +1,46 @@
 #pragma once
 
+#ifdef FIREBOX_PLATFORM_WIN64
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
 #define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
-
-#ifdef FIREBOX_PLATFORM_WIN64
 
 extern Firebox::Application* Firebox::CreateApp();
 
 int main(int argc, char** argv)
 {
+
+#ifdef defined(__linux__)
+#include <cstdlib>
+#include <unistd.h>
+
+	if (getenv("__INTERNAL_GPU_FORCED") == nullptr)
+	{
+		setenv("__NV_PRIME_RENDER_OFFLOAD", "1", 1);
+		setenv("__GLX_VENDOR_LIBRARY_NAME", "nvidia", 1);
+		
+		setenv("DRI_PRIME", "1", 1);
+		setenv("__INTERNAL_GPU_FORCED", "1", 1);
+
+		execvp(argv[0], argv);
+		std::cerr << "Failed to force dedicated GPU environment\n";
+	}
+
+#endif
+
+#ifdef FIREBOX_PLATFORM_WIN64
+
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Firebox::EngineContent::Init();
 	FireboxEditor::EditorContent::Init();
@@ -17,6 +49,6 @@ int main(int argc, char** argv)
 	app->Run();
 	_CrtDumpMemoryLeaks();
 	delete app;
-}
 
 #endif
+}
